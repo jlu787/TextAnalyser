5cb0a75726f95cb059075dcb74255461f590a767
jlu787 <jlu787@outlook.com>
2017-07-07 17:26:03 +1200

    3 - second page with some unfunctional API

---
+++ TextAnalyser/SendText.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TextAnalyser
+{
+	public class Document
+	{
+		public string language { get; set; }
+		public string id { get; set; }
+		public string text { get; set; }
+	}
+
+	public class SendText
+	{
+		public List<Document> documents { get; set; }
+	}
+}
--- TextAnalyser/TextAnalyserPage.xaml.cs
+++ TextAnalyser/TextAnalyserPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Xamarin.Forms;
 
 namespace TextAnalyser
 {
@@ -7,6 +11,40 @@ namespace TextAnalyser
         public TextAnalyserPage()
         {
             InitializeComponent();
+
+
         }
+
+        async void clickAnalyseAsync(object sender, EventArgs args)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:6740");
+
+                var sendText = new SendText();
+                var document = new Document();
+
+                document.language = "en";
+                document.id = "1";
+                document.text = textToBeAnalysed.Text;
+
+                sendText.documents.Add(document);
+
+                var content = new FormUrlEncodedContent(sendText);
+                var result = await client.PostAsync("/text/analytics/v2.0/sentiment", JsonConvert.DeserializeObject(sendText));
+                string resultContent = await result.Content.ReadAsStringAsync();
+            }
+            //DisplayAlert("Click", "Analyse was clicked", "Ok");
+        }
+
+        void clickReset(object sender, EventArgs args)
+		{
+            DisplayAlert("Click", "Reset was clicked", "Ok");
+		}
+
+		void clickHistory(object sender, EventArgs args)
+		{
+            DisplayAlert("Click", "History was clicked", "Ok");
+		}
     }
 }
